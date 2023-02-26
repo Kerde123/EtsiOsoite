f8104053a536be4668530d3a31a1a3cfcf307fd5
Kerde123 <kerttu.vanhala@gmail.com>
2023-02-26 12:15:12 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,12 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Alert, Button } from 'react-native';
+import { API_TOKEN } from '@env';
+import MapView, { Marker } from 'react-native-maps';
+import { useEffect, useState } from 'react';
+
+
+// geokoodaus, osoitepalvelu karttapalvelun sisässä
+// esim. mapquest-palvelussa. haetaan osoitteella -> hakee koordinaattien mukaan
+// alkuun initial region, jota näytetään, ennen kuin käyttäjä tekee haun
+// kun kirjottaa osoitteen -> sillä tehdään geokoodauspyyntö -> saadaan koordinaatit -> asetetaan ne koordinaatit mapview:lle, 
+// niin että se mapview päivittyy siihen mitä halutaan näyttää eli siihen osoitteeseen
+
 
 export default function App() {
+
+  const [osoite, setOsoite] = useState('');
+  const [coordinates, setCoordinates] = useState({});
+
+      
+  const getCoordinates = () => {
+      fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=${API_TOKEN}&location=${osoite}`)
+      .then(response => response.json())
+      .then(responseJson => setCoordinates(responseJson.results[0].locations.latLng))
+      .catch(error => { 
+        Alert.alert('Error', error.message); 
+      });  
+      console.log(Object.keys(coordinates)); 
+      } 
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <MapView
+        style={{ width: '100%', height: '90%' }}
+        initialRegion={{
+          latitude: 60.15524,
+          longitude: 24.9117114,
+          latitudeDelta: 0.0322,
+          longitudeDelta: 0.0221
+        }}>
+      </MapView>
+      <TextInput
+        placeholder={'Osoite'}
+        value={osoite}
+        onChangeText={text => setOsoite(text)}
+        />
+        <Button 
+      title = "Show" 
+      onPress = {getCoordinates} 
+        />
+  
+        <MapView 
+            style={{ width: '100%', height: '100%' }}
+            region= {{
+              latitude: coordinates[0],
+              longitude: coordinates[1],
+              latitudeDelta: 0.0322,
+              longitudeDelta: 0.0221
+            }}
+            >
+            <Marker 
+              coordinate={{
+              latitude: coordinates[0],
+              longitude: coordinates[1]
+              }}
+              />
+          </MapView> 
+        </View>
   );
 }
 
